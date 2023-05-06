3d03cc047bda6b3ee8b1c8d31d2de1dad025dc6c
NihithaAvula <86791582+NihithaAvula@users.noreply.github.com>
2023-05-06 10:15:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
-   // song1 = loadSound(" ");
-    //song2 = loadSound(" ");
+   song1 = loadSound("music.mp3");
+   song2 = loadSound("music2.mp3");
 }
 
 function setup(){
@@ -19,5 +23,19 @@ function draw(){
 }
 
 function play(){
-   // song1.play();
+   song1.play();
+   song1.setVolume(1);
+   song1.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("left wrist X =" + leftWristX + ", left Wrist Y ="+ leftWristY);
+        console.log("right wrist X =" + rightWristX + ", right Wrist Y ="+ rightWristY);
+    }
 }
